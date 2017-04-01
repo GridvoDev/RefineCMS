56f7e1d6e2dccabe39169d2d467faa6492274d10
JeMoGeS <941206332@qq.com>
2017-04-01 22:01:21 +0800

    modify keystone.js and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell')
@@ -16,3 +17,23 @@ gulp.task('watch', [
 ]);
 
 gulp.task('default', ['watch', 'runKeystone']);
+=======
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
+
+
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+
+};
+
+
+
+gulp.task('runKeystone', shell.task('node keystone.js'));
+gulp.task('watch', [
+
+]);
+
+gulp.task('default', ['watch', 'runKeystone']);
+>>>>>>> b0eb7369478abac56cd444606cf895bb4ced42e0
